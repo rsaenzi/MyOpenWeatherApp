5727fc80b7480f1a68658d165ef4230ba070145c
Rigoberto Sáenz Imbacuán <beto456789@gmail.com>
2017-06-04 18:19:14 -0500

    - Project created

---
+++ MyOpenWeatherApp/MyOpenWeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyOpenWeatherApp
+//
+//  Created by Rigoberto Sáenz Imbacuán on 6/4/17.
+//  Copyright © 2017 Rigoberto Sáenz Imbacuán. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
